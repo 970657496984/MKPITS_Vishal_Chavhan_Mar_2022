a0d200ff99dd7ed135d2a717639ab7ddf4ec60c3
970657496984 <vishalramchavhan1245@gmail.com>
2022-03-30 12:30:23 -0400

    addition of two number

---
+++ 30 Mar/ADD.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int num1,num2,res;
+clrscr();
+printf("enter the two integer number\n");
+scanf("%d%d",&num1,&num2);
+res=num1+num2;
+printf("result of addition= %d",res);
+getch();
+}
