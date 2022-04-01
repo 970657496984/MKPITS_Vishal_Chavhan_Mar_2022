245a048e539a22f4f86980a37a7ba1e61e210c86
970657496984 <vishalramchavhan1245@gmail.com>
2022-04-01 07:32:54 +0530

    area of rectangle

---
+++ 31 March/FP2.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int l,b,area;
+clrscr();
+printf("enterlength and width of rectangular\n");
+scanf("%d%d",&l,&b);
+area=l*b;
+printf("area of rectangle=%d",area);
+getch();
+}
